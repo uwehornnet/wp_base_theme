26de92af3e3bdbb3bafa047586edd2e2f33768a8
Uwe Horn <uwehorn@Uwes-MBP.fritz.box>
2018-11-06 18:14:58 +0100

    added example page-templates

---
+++ page__boxed.php
@@ -0,0 +1,13 @@
+<?php
+/*
+* Template Name: Boxed
+*/
+
+get_header(); ?>
+
+<?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>
+    <?php the_title()?>
+    <?php the_content(); ?>
+<?php endwhile; endif; wp_reset_query(); ?>
+
+<?php get_footer(); ?>
